f0fb2353b22a3170d62c40a123ad490e4e13c201
Ruben Ignacio <ruben.ignaciogz@gmail.com>
2018-10-05 21:01:58 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,25 @@
+# WebExtensionLibs
+
+## StorageManager
+```javascript
+var storage = new StorageManager();
+// muestra el objeto de clave/valor con todos los items almacenados.
+storage.get(null).then((items) => console.log(items));
+// almacena uno o mas items en el storage.
+storage.set({item1: "Hola mundo"}).then(() => console.log("Se almaceno correctamente."));
+```
+Por defecto StorageManager usa el storage 'local', si en caso quiere usar otro storage como 'sync' pude pasar como segundo parámetro el tipo de almacenamiento a usar.
+```javascript
+// obtiene todos los items almacenados en 'sync'.
+storage.get(null, 'sync').then((items) => console.log(items));
+```
+Tambien establecer el almacenamiento por defecto que prefiera.
+```javascript
+// puede usar el metodo 'setDefaultStorageArea' para cambiar el almacenamiento por defecto.
+storage.setDefaultStorageArea('sync');
+// o tambien hacerlo al crear la instancia.
+var storageSync = new StorageManager('sync');
+```
+## License
+
+[MIT License](https://opensource.org/licenses/MIT).
---
+++ StorageManager.js
@@ -0,0 +1,69 @@
+function StorageManager(defaultStorageArea) {
+  if (!(this instanceof StorageManager))
+    return new StorageManager(defaultStorageArea);
+
+  this.browserStorage = StorageManager.browserStorage;
+  if (!this.browserStorage)
+    throw new Error("Your browser does not support storage.");
+  this.setDefaultStorageArea(defaultStorageArea);
+}
+
+StorageManager.browserStorage = (window.browser || window.chrome).storage;
+
+StorageManager.StorageArea = {LOCAL: "local", SYNC: "sync", MANAGED: "managed"};
+
+StorageManager.prototype.setDefaultStorageArea = function(newDefaultStorageArea = StorageManager.StorageArea.LOCAL) {
+  var storageAreas = Object.values(StorageManager.StorageArea);
+  if (!storageAreas.includes(newDefaultStorageArea))
+    throw new RangeError("Storage area '" + newDefaultStorageArea + "' is invalid.");
+
+  if (!this.browserStorage[newDefaultStorageArea])
+    throw new RangeError("Your browser does not support 'Storage." + newDefaultStorageArea + "'.");
+  this.defaultStorageArea = newDefaultStorageArea;
+  this.getStorage();
+};
+
+StorageManager.prototype.getStorage = function(storageArea = this.defaultStorageArea) {
+  var storageAreas = Object.values(StorageManager.StorageArea);
+  if (!storageAreas.includes(storageArea))
+    throw new RangeError("Storage area '" + storageArea + "' is invalid.");
+
+  var storage = this.browserStorage[storageArea];
+  if (!storage)
+    throw new RangeError("Your browser does not support 'Storage." + storageArea + "'.");
+  return storage;
+};
+
+StorageManager.prototype.getStorageMethod = function(name, storageArea = this.defaultStorageArea) {
+  var storageMethod = this.getStorage(storageArea)[name];
+  if (!storageMethod)
+    throw new Error("Your browser does not support 'Storage." + storageArea + "." + name + "()'.");
+  return function() {
+    if (window.browser)
+      return storageMethod.apply(null, arguments);
+    else {
+      var args = Array.prototype.slice.call(arguments);
+      return new Promise(function(resolve, reject) {
+        function callback() {
+          var runtimeError = chrome.runtime.lastError;
+          if (runtimeError)
+            reject(runtimeError);
+          else
+            resolve.apply(null, arguments);
+        }
+        args.push(callback);
+        storageMethod.apply(null, args);
+      });
+    }
+  };
+};
+
+["get", "getBytesInUse", "set", "remove"].forEach(function(methodName) {
+  StorageManager.prototype[methodName] = function(keysItems, storageArea) {
+    return this.getStorageMethod(methodName, storageArea)(keysItems);
+  };
+});
+
+StorageManager.prototype.clear = function(storageArea) {
+  return this.getStorageMethod("clear", storageArea)();
+};
