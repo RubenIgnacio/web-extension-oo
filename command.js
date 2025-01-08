3c4f23bed267020f2926b8c9dbd7105956d23eee
Ruben Ignacio Guzman Zamora <ruben.ignaciogz@gmail.com>
2025-01-08 04:25:55 -0500

    Replace Command.js with command.js and extend ClassExtensionBase and improve API method handling

---
+++ command.js
@@ -0,0 +1,69 @@
+import { getAPI, ClassExtensionBase } from './extension-api';
+
+export default class Command extends ClassExtensionBase {
+  static metadata =  {
+    getAll: {
+      minArgs: 0,
+      maxArgs: 0
+    }
+  };
+  static apiName = 'commands';
+  static fields = [ 'description', 'name', 'shortcut' ];
+
+  constructor(options) {
+    this.assignFields(options);
+  }
+
+  static getAll() {
+    const apiMethod = this.getAPIMethod('getAll');
+
+    return apiMethod().then((commands) => {
+      return commands.map((command) => new this(command));
+    });
+  }
+
+  static get(name) {
+    const apiMethod = this.getAPIMethod('getAll');
+
+    return apiMethod().then((commands) => {
+      const command = commands.find((command) => command.name === name);
+
+      return command ? new this(command) : null;
+    });
+  }
+
+  getAPIMethod(name) {
+    return this.constructor.getAPIMethod(name);
+  }
+
+  reload() {
+    const apiMethod = this.getAPIMethod('getAll');
+
+    apiMethod().then((commands) => {
+      const command = commands.find((command) => command.name === this.name);
+
+      if (!command) {
+        throw new Error(`Command '${this.name}' was not found.`);
+      }
+
+      for (const field of this.constructor.fields) {
+        if (field in command) {
+          this[field] = command[field];
+        }
+      }
+    });
+  }
+
+  reset() {
+    const apiMethod = this.getAPIMethod('reset');
+
+    return apiMethod(this.name).then(() => this.reload());
+  }
+
+  update(details) {
+    details.name = this.name;
+    const apiMethod = this.getAPIMethod('update');
+
+    return apiMethod(details).then(() => this.reload());
+  }
+}
