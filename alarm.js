746ef13bfff8a133bb40a4b717f69df49636e748
Ruben Ignacio Guzman Zamora <ruben.ignaciogz@gmail.com>
2025-01-08 04:10:29 -0500

    Replace Alarm.js with alarm.js and extend ClassExtensionBase and improve API method handling

---
+++ alarm.js
@@ -0,0 +1,70 @@
+import { getAPI, ClassExtensionBase } from './extension-api';
+
+export default class Alarm extends ClassExtensionBase {
+  static metadata = {
+    clear: {
+      minArgs: 0,
+      maxArgs: 1
+    },
+    clearAll: {
+      minArgs: 0,
+      maxArgs: 0
+    },
+    get: {
+      minArgs: 0,
+      maxArgs: 1
+    },
+    getAll: {
+      minArgs: 0,
+      maxArgs: 0
+    }
+  };
+  static apiName = 'alarms';
+  static fields = ['name', 'delayInMinutes', 'periodInMinutes', 'when'];
+
+  constructor(alarmInfo) {
+    this.assignFields(alarmInfo);
+  }
+
+  static create(name, alarmInfo) {
+    return this.getAPIMethod('create')(name, alarmInfo);
+  }
+
+  static getAll() {
+    const apiMethod = this.getAPIMethod('getAll');
+
+    return apiMethod().then((alarmsArray) => {
+      return alarmsArray.map((alarm) => new this(alarm));
+    });
+  }
+
+  static get(name) {
+    const apiMethod = this.getAPIMethod('get');
+
+    return apiMethod(name).then((alarmInfo) => {
+      if (typeof alarmInfo !== 'object') return;
+
+      return new this(alarmInfo);
+    });
+  }
+
+  static clearAll() {
+    return this.getAPIMethod('clearAll')();
+  }
+
+  getAPIMethod(name) {
+    return this.constructor.getAPIMethod(name);
+  }
+
+  clear() {
+    return this.getAPIMethod('clear')(this.name);
+  }
+
+  addEventListener(type, listener) {
+    this.constructor.addEventListener(type, (alarm) => {
+      if (alarm.name !== this.name) return;
+
+      listener(this);
+    });
+  }
+}
