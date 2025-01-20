9422cefa1cb5f4721aa48d36a7dc7f840dd755e0
Ruben Ignacio Guzman Zamora <ruben.ignaciogz@gmail.com>
2025-01-20 17:08:45 -0500

    Add index.js for module exports

---
+++ index.js
@@ -0,0 +1,21 @@
+import { webBrowser, wrapAPI, getAPIEvent, ClassExtensionBase } from './lib/extension-api.js';
+
+import Alarm from './lib/alarm.js';
+import BrowserWindow from './lib/browser-window.js';
+import Command from './lib/command.js';
+import Notification from './lib/notification.js';
+import StorageArea from './lib/storage-area.js';
+import Tab from './lib/tab.js';
+
+export {
+  Alarm,
+  BrowserWindow,
+  Command,
+  Notification,
+  StorageArea,
+  Tab,
+  webBrowser,
+  wrapAPI,
+  getAPIEvent,
+  ClassExtensionBase
+};
